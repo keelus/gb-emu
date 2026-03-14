56b21dc7bdfe1471394cfdaa267d0fbbdef32d2d
keelus <hugomoreda@hotmail.com>
2026-03-14 17:40:34 +0100

    Implement the rest of the prefixed instructions.

--- src/cpu_cb_instructions.cpp
+++ src/cpu_cb_instructions.cpp
@@ -732,6 +732,582 @@ int Cpu::executeCbInstruction(void) {
 	case 0x3F: // SRL A
 		doSrl(m_A);
 		break;
+	case 0x40: // BIT 0, B
+		doBit(m_B, 0);
+		break;
+	case 0x41: // BIT 0, C
+		doBit(m_C, 0);
+		break;
+	case 0x42: // BIT 0, D
+		doBit(m_D, 0);
+		break;
+	case 0x43: // BIT 0, E
+		doBit(m_E, 0);
+		break;
+	case 0x44: // BIT 0, H
+		doBit(m_H, 0);
+		break;
+	case 0x45: // BIT 0, L
+		doBit(m_L, 0);
+		break;
+	case 0x46: // BIT 0, [HL]
+		doBit(HL(), 0);
+		break;
+	case 0x47: // BIT 0, A
+		doBit(m_A, 0);
+		break;
+	case 0x48: // BIT 1, B
+		doBit(m_B, 1);
+		break;
+	case 0x49: // BIT 1, C
+		doBit(m_C, 1);
+		break;
+	case 0x4A: // BIT 1, D
+		doBit(m_D, 1);
+		break;
+	case 0x4B: // BIT 1, E
+		doBit(m_E, 1);
+		break;
+	case 0x4C: // BIT 1, H
+		doBit(m_H, 1);
+		break;
+	case 0x4D: // BIT 1, L
+		doBit(m_L, 1);
+		break;
+	case 0x4E: // BIT 1, [HL]
+		doBit(HL(), 1);
+		break;
+	case 0x4F: // BIT 1, A
+		doBit(m_A, 1);
+		break;
+	case 0x50: // BIT 2, B
+		doBit(m_B, 2);
+		break;
+	case 0x51: // BIT 2, C
+		doBit(m_C, 2);
+		break;
+	case 0x52: // BIT 2, D
+		doBit(m_D, 2);
+		break;
+	case 0x53: // BIT 2, E
+		doBit(m_E, 2);
+		break;
+	case 0x54: // BIT 2, H
+		doBit(m_H, 2);
+		break;
+	case 0x55: // BIT 2, L
+		doBit(m_L, 2);
+		break;
+	case 0x56: // BIT 2, [HL]
+		doBit(HL(), 2);
+		break;
+	case 0x57: // BIT 2, A
+		doBit(m_A, 2);
+		break;
+	case 0x58: // BIT 3, B
+		doBit(m_B, 3);
+		break;
+	case 0x59: // BIT 3, C
+		doBit(m_C, 3);
+		break;
+	case 0x5A: // BIT 3, D
+		doBit(m_D, 3);
+		break;
+	case 0x5B: // BIT 3, E
+		doBit(m_E, 3);
+		break;
+	case 0x5C: // BIT 3, H
+		doBit(m_H, 3);
+		break;
+	case 0x5D: // BIT 3, L
+		doBit(m_L, 3);
+		break;
+	case 0x5E: // BIT 3, [HL]
+		doBit(HL(), 3);
+		break;
+	case 0x5F: // BIT 3, A
+		doBit(m_A, 3);
+		break;
+	case 0x60: // BIT 4, B
+		doBit(m_B, 4);
+		break;
+	case 0x61: // BIT 4, C
+		doBit(m_C, 4);
+		break;
+	case 0x62: // BIT 4, D
+		doBit(m_D, 4);
+		break;
+	case 0x63: // BIT 4, E
+		doBit(m_E, 4);
+		break;
+	case 0x64: // BIT 4, H
+		doBit(m_H, 4);
+		break;
+	case 0x65: // BIT 4, L
+		doBit(m_L, 4);
+		break;
+	case 0x66: // BIT 4, [HL]
+		doBit(HL(), 4);
+		break;
+	case 0x67: // BIT 4, A
+		doBit(m_A, 4);
+		break;
+	case 0x68: // BIT 5, B
+		doBit(m_B, 5);
+		break;
+	case 0x69: // BIT 5, C
+		doBit(m_C, 5);
+		break;
+	case 0x6A: // BIT 5, D
+		doBit(m_D, 5);
+		break;
+	case 0x6B: // BIT 5, E
+		doBit(m_E, 5);
+		break;
+	case 0x6C: // BIT 5, H
+		doBit(m_H, 5);
+		break;
+	case 0x6D: // BIT 5, L
+		doBit(m_L, 5);
+		break;
+	case 0x6E: // BIT 5, [HL]
+		doBit(HL(), 5);
+		break;
+	case 0x6F: // BIT 5, A
+		doBit(m_A, 5);
+		break;
+	case 0x70: // BIT 6, B
+		doBit(m_B, 6);
+		break;
+	case 0x71: // BIT 6, C
+		doBit(m_C, 6);
+		break;
+	case 0x72: // BIT 6, D
+		doBit(m_D, 6);
+		break;
+	case 0x73: // BIT 6, E
+		doBit(m_E, 6);
+		break;
+	case 0x74: // BIT 6, H
+		doBit(m_H, 6);
+		break;
+	case 0x75: // BIT 6, L
+		doBit(m_L, 6);
+		break;
+	case 0x76: // BIT 6, [HL]
+		doBit(HL(), 6);
+		break;
+	case 0x77: // BIT 6, A
+		doBit(m_A, 6);
+		break;
+	case 0x78: // BIT 7, B
+		doBit(m_B, 7);
+		break;
+	case 0x79: // BIT 7, C
+		doBit(m_C, 7);
+		break;
+	case 0x7A: // BIT 7, D
+		doBit(m_D, 7);
+		break;
+	case 0x7B: // BIT 7, E
+		doBit(m_E, 7);
+		break;
+	case 0x7C: // BIT 7, H
+		doBit(m_H, 7);
+		break;
+	case 0x7D: // BIT 7, L
+		doBit(m_L, 7);
+		break;
+	case 0x7E: // BIT 7, [HL]
+		doBit(HL(), 7);
+		break;
+	case 0x7F: // BIT 7, A
+		doBit(m_A, 7);
+		break;
+	case 0x80: // RES 0, B
+		doRes(m_B, 0);
+		break;
+	case 0x81: // RES 0, C
+		doRes(m_C, 0);
+		break;
+	case 0x82: // RES 0, D
+		doRes(m_D, 0);
+		break;
+	case 0x83: // RES 0, E
+		doRes(m_E, 0);
+		break;
+	case 0x84: // RES 0, H
+		doRes(m_H, 0);
+		break;
+	case 0x85: // RES 0, L
+		doRes(m_L, 0);
+		break;
+	case 0x86: // RES 0, [HL]
+		doRes(HL(), 0);
+		break;
+	case 0x87: // RES 0, A
+		doRes(m_A, 0);
+		break;
+	case 0x88: // RES 1, B
+		doRes(m_B, 1);
+		break;
+	case 0x89: // RES 1, C
+		doRes(m_C, 1);
+		break;
+	case 0x8A: // RES 1, D
+		doRes(m_D, 1);
+		break;
+	case 0x8B: // RES 1, E
+		doRes(m_E, 1);
+		break;
+	case 0x8C: // RES 1, H
+		doRes(m_H, 1);
+		break;
+	case 0x8D: // RES 1, L
+		doRes(m_L, 1);
+		break;
+	case 0x8E: // RES 1, [HL]
+		doRes(HL(), 1);
+		break;
+	case 0x8F: // RES 1, A
+		doRes(m_A, 1);
+		break;
+	case 0x90: // RES 2, B
+		doRes(m_B, 2);
+		break;
+	case 0x91: // RES 2, C
+		doRes(m_C, 2);
+		break;
+	case 0x92: // RES 2, D
+		doRes(m_D, 2);
+		break;
+	case 0x93: // RES 2, E
+		doRes(m_E, 2);
+		break;
+	case 0x94: // RES 2, H
+		doRes(m_H, 2);
+		break;
+	case 0x95: // RES 2, L
+		doRes(m_L, 2);
+		break;
+	case 0x96: // RES 2, [HL]
+		doRes(HL(), 2);
+		break;
+	case 0x97: // RES 2, A
+		doRes(m_A, 2);
+		break;
+	case 0x98: // RES 3, B
+		doRes(m_B, 3);
+		break;
+	case 0x99: // RES 3, C
+		doRes(m_C, 3);
+		break;
+	case 0x9A: // RES 3, D
+		doRes(m_D, 3);
+		break;
+	case 0x9B: // RES 3, E
+		doRes(m_E, 3);
+		break;
+	case 0x9C: // RES 3, H
+		doRes(m_H, 3);
+		break;
+	case 0x9D: // RES 3, L
+		doRes(m_L, 3);
+		break;
+	case 0x9E: // RES 3, [HL]
+		doRes(HL(), 3);
+		break;
+	case 0x9F: // RES 3, A
+		doRes(m_A, 3);
+		break;
+	case 0xA0: // RES 4, B
+		doRes(m_B, 4);
+		break;
+	case 0xA1: // RES 4, C
+		doRes(m_C, 4);
+		break;
+	case 0xA2: // RES 4, D
+		doRes(m_D, 4);
+		break;
+	case 0xA3: // RES 4, E
+		doRes(m_E, 4);
+		break;
+	case 0xA4: // RES 4, H
+		doRes(m_H, 4);
+		break;
+	case 0xA5: // RES 4, L
+		doRes(m_L, 4);
+		break;
+	case 0xA6: // RES 4, [HL]
+		doRes(HL(), 4);
+		break;
+	case 0xA7: // RES 4, A
+		doRes(m_A, 4);
+		break;
+	case 0xA8: // RES 5, B
+		doRes(m_B, 5);
+		break;
+	case 0xA9: // RES 5, C
+		doRes(m_C, 5);
+		break;
+	case 0xAA: // RES 5, D
+		doRes(m_D, 5);
+		break;
+	case 0xAB: // RES 5, E
+		doRes(m_E, 5);
+		break;
+	case 0xAC: // RES 5, H
+		doRes(m_H, 5);
+		break;
+	case 0xAD: // RES 5, L
+		doRes(m_L, 5);
+		break;
+	case 0xAE: // RES 5, [HL]
+		doRes(HL(), 5);
+		break;
+	case 0xAF: // RES 5, A
+		doRes(m_A, 5);
+		break;
+	case 0xB0: // RES 6, B
+		doRes(m_B, 6);
+		break;
+	case 0xB1: // RES 6, C
+		doRes(m_C, 6);
+		break;
+	case 0xB2: // RES 6, D
+		doRes(m_D, 6);
+		break;
+	case 0xB3: // RES 6, E
+		doRes(m_E, 6);
+		break;
+	case 0xB4: // RES 6, H
+		doRes(m_H, 6);
+		break;
+	case 0xB5: // RES 6, L
+		doRes(m_L, 6);
+		break;
+	case 0xB6: // RES 6, [HL]
+		doRes(HL(), 6);
+		break;
+	case 0xB7: // RES 6, A
+		doRes(m_A, 6);
+		break;
+	case 0xB8: // RES 7, B
+		doRes(m_B, 7);
+		break;
+	case 0xB9: // RES 7, C
+		doRes(m_C, 7);
+		break;
+	case 0xBA: // RES 7, D
+		doRes(m_D, 7);
+		break;
+	case 0xBB: // RES 7, E
+		doRes(m_E, 7);
+		break;
+	case 0xBC: // RES 7, H
+		doRes(m_H, 7);
+		break;
+	case 0xBD: // RES 7, L
+		doRes(m_L, 7);
+		break;
+	case 0xBE: // RES 7, [HL]
+		doRes(HL(), 7);
+		break;
+	case 0xBF: // RES 7, A
+		doRes(m_A, 7);
+		break;
+	case 0xC0: // SET 0, B
+		doSet(m_B, 0);
+		break;
+	case 0xC1: // SET 0, C
+		doSet(m_C, 0);
+		break;
+	case 0xC2: // SET 0, D
+		doSet(m_D, 0);
+		break;
+	case 0xC3: // SET 0, E
+		doSet(m_E, 0);
+		break;
+	case 0xC4: // SET 0, H
+		doSet(m_H, 0);
+		break;
+	case 0xC5: // SET 0, L
+		doSet(m_L, 0);
+		break;
+	case 0xC6: // SET 0, [HL]
+		doSet(HL(), 0);
+		break;
+	case 0xC7: // SET 0, A
+		doSet(m_A, 0);
+		break;
+	case 0xC8: // SET 1, B
+		doSet(m_B, 1);
+		break;
+	case 0xC9: // SET 1, C
+		doSet(m_C, 1);
+		break;
+	case 0xCA: // SET 1, D
+		doSet(m_D, 1);
+		break;
+	case 0xCB: // SET 1, E
+		doSet(m_E, 1);
+		break;
+	case 0xCC: // SET 1, H
+		doSet(m_H, 1);
+		break;
+	case 0xCD: // SET 1, L
+		doSet(m_L, 1);
+		break;
+	case 0xCE: // SET 1, [HL]
+		doSet(HL(), 1);
+		break;
+	case 0xCF: // SET 1, A
+		doSet(m_A, 1);
+		break;
+	case 0xD0: // SET 2, B
+		doSet(m_B, 2);
+		break;
+	case 0xD1: // SET 2, C
+		doSet(m_C, 2);
+		break;
+	case 0xD2: // SET 2, D
+		doSet(m_D, 2);
+		break;
+	case 0xD3: // SET 2, E
+		doSet(m_E, 2);
+		break;
+	case 0xD4: // SET 2, H
+		doSet(m_H, 2);
+		break;
+	case 0xD5: // SET 2, L
+		doSet(m_L, 2);
+		break;
+	case 0xD6: // SET 2, [HL]
+		doSet(HL(), 2);
+		break;
+	case 0xD7: // SET 2, A
+		doSet(m_A, 2);
+		break;
+	case 0xD8: // SET 3, B
+		doSet(m_B, 3);
+		break;
+	case 0xD9: // SET 3, C
+		doSet(m_C, 3);
+		break;
+	case 0xDA: // SET 3, D
+		doSet(m_D, 3);
+		break;
+	case 0xDB: // SET 3, E
+		doSet(m_E, 3);
+		break;
+	case 0xDC: // SET 3, H
+		doSet(m_H, 3);
+		break;
+	case 0xDD: // SET 3, L
+		doSet(m_L, 3);
+		break;
+	case 0xDE: // SET 3, [HL]
+		doSet(HL(), 3);
+		break;
+	case 0xDF: // SET 3, A
+		doSet(m_A, 3);
+		break;
+	case 0xE0: // SET 4, B
+		doSet(m_B, 4);
+		break;
+	case 0xE1: // SET 4, C
+		doSet(m_C, 4);
+		break;
+	case 0xE2: // SET 4, D
+		doSet(m_D, 4);
+		break;
+	case 0xE3: // SET 4, E
+		doSet(m_E, 4);
+		break;
+	case 0xE4: // SET 4, H
+		doSet(m_H, 4);
+		break;
+	case 0xE5: // SET 4, L
+		doSet(m_L, 4);
+		break;
+	case 0xE6: // SET 4, [HL]
+		doSet(HL(), 4);
+		break;
+	case 0xE7: // SET 4, A
+		doSet(m_A, 4);
+		break;
+	case 0xE8: // SET 5, B
+		doSet(m_B, 5);
+		break;
+	case 0xE9: // SET 5, C
+		doSet(m_C, 5);
+		break;
+	case 0xEA: // SET 5, D
+		doSet(m_D, 5);
+		break;
+	case 0xEB: // SET 5, E
+		doSet(m_E, 5);
+		break;
+	case 0xEC: // SET 5, H
+		doSet(m_H, 5);
+		break;
+	case 0xED: // SET 5, L
+		doSet(m_L, 5);
+		break;
+	case 0xEE: // SET 5, [HL]
+		doSet(HL(), 5);
+		break;
+	case 0xEF: // SET 5, A
+		doSet(m_A, 5);
+		break;
+	case 0xF0: // SET 6, B
+		doSet(m_B, 6);
+		break;
+	case 0xF1: // SET 6, C
+		doSet(m_C, 6);
+		break;
+	case 0xF2: // SET 6, D
+		doSet(m_D, 6);
+		break;
+	case 0xF3: // SET 6, E
+		doSet(m_E, 6);
+		break;
+	case 0xF4: // SET 6, H
+		doSet(m_H, 6);
+		break;
+	case 0xF5: // SET 6, L
+		doSet(m_L, 6);
+		break;
+	case 0xF6: // SET 6, [HL]
+		doSet(HL(), 6);
+		break;
+	case 0xF7: // SET 6, A
+		doSet(m_A, 6);
+		break;
+	case 0xF8: // SET 7, B
+		doSet(m_B, 7);
+		break;
+	case 0xF9: // SET 7, C
+		doSet(m_C, 7);
+		break;
+	case 0xFA: // SET 7, D
+		doSet(m_D, 7);
+		break;
+	case 0xFB: // SET 7, E
+		doSet(m_E, 7);
+		break;
+	case 0xFC: // SET 7, H
+		doSet(m_H, 7);
+		break;
+	case 0xFD: // SET 7, L
+		doSet(m_L, 7);
+		break;
+	case 0xFE: // SET 7, [HL]
+		doSet(HL(), 7);
+		break;
+	case 0xFF: // SET 7, A
+		doSet(m_A, 7);
+		break;
 	default: {
 		std::stringstream ss;
 		ss << "Unhandled prefixed instruction 0x" << std::hex << std::setw(2) << std::setfill('0') << int(opcode)
