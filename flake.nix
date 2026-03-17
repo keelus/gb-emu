b6cd8ea994f518ecd500697bb54f73661474c98a
keelus <hugomoreda@hotmail.com>
2026-03-17 20:20:59 +0100

    Replace nix shell with nix flake.

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+	description = "gb-emu development flake";
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+	};
+
+	outputs = {
+		self,
+		nixpkgs,
+	}: let
+		pkgs = nixpkgs.legacyPackages.x86_64-linux;
+	in {
+		devShells.x86_64-linux.default =
+			pkgs.mkShell {
+				buildInputs = with pkgs; [
+					valgrind
+					gcc
+					gnumake
+					cmake
+					pkg-config
+					glib
+					SDL2
+					SDL2_ttf
+					harfbuzz
+				];
+			};
+	};
+}
