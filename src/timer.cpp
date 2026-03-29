eb52d0afb2bbb5ddbf71102909ece48687731bb3
keelus <hugomoreda@hotmail.com>
2026-03-29 12:34:56 +0200

    Improve timer's accuracy and behavior.

--- src/timer.cpp
+++ src/timer.cpp
@@ -1,18 +1,36 @@
 #include "timer.hpp"
 #include "bus.hpp"
+#include <cstdint>
 
 void Timer::tickOne() {
-	m_div++;
-	if(!isTimaEnabled()) { return; }
+	m_clk++;
 
-	m_timaAccTStates++;
+	if(!isTimaEnabled()) {
+		m_prevClk = m_clk;
+		return;
+	}
 
-	if(m_timaAccTStates >= timaTStatesPerIncrement()) {
-		m_timaAccTStates %= timaTStatesPerIncrement();
+	uint16_t mask;
+	uint8_t mode = m_tac & 0x3;
+	bool timaIncrements = false;
+	switch(mode) {
+	case 0b00: mask = 1 << 9; break;
+	case 0b01: mask = 1 << 3; break;
+	case 0b10: mask = 1 << 5; break;
+	case 0b11: mask = 1 << 7; break;
+	}
 
-		if(++m_tima == 0) {
+	uint16_t curBit = (m_clk & mask) != 0;
+	uint16_t prevBit = (m_prevClk & mask) != 0;
+
+	if(prevBit == 1 && curBit == 0) {
+		m_tima++;
+
+		if(m_tima == 0) {
 			m_tima = m_tma;
 			m_bus.requestInterrupt(Bus::InterruptRequestType::Timer);
 		}
 	}
+
+	m_prevClk = m_clk;
 }
--- src/timer.hpp
+++ src/timer.hpp
@@ -8,13 +8,12 @@ class Bus;
 class Timer {
   public:
 	Timer(Bus &bus) : m_bus(bus) {
-		m_div = 0xAB;
 		m_tac = 0;
 		m_tma = 0;
 		m_tima = 0;
-		m_timaAccTStates = 0;
 
-		m_tStateAcc = 0;
+		m_clk = 0;
+		m_prevClk = 0;
 	}
 
 	void tick(const uint8_t tStates) {
@@ -23,8 +22,8 @@ class Timer {
 		}
 	}
 
-	void resetDiv() { m_div = 0; }
-	uint8_t getDiv() const { return m_div; }
+	void resetDiv() { m_clk = 0; }
+	uint8_t getDiv() const { return static_cast<uint8_t>(m_clk >> 8); }
 
 	uint8_t getTac() const { return m_tac; }
 	void setTac(const uint8_t newTac) { m_tac = newTac & 0x7; }
@@ -40,28 +39,12 @@ class Timer {
 
 	bool isTimaEnabled() const { return (m_tac & 0x4) != 0; }
 
-	uint16_t timaTStatesPerIncrement() const {
-		uint8_t mode = m_tac & 0x3;
-		switch(mode) {
-		case 0b00: return 1024;
-		case 0b01: return 16;
-		case 0b10: return 64;
-		case 0b11: return 256;
-		}
-
-		return 0;
-	}
-
-	uint8_t m_div;
-
 	uint8_t m_tac;
 
 	uint8_t m_tma;
-
-	uint16_t m_timaAccTStates;
 	uint8_t m_tima;
 
-	size_t m_tStateAcc;
+	uint16_t m_clk, m_prevClk;
 
 	Bus &m_bus;
 };
