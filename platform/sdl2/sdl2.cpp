ef61bd79dae8ff50e8376ea8521914c78df94d15
keelus <hugomoreda@hotmail.com>
2026-05-04 21:18:28 +0200

    Remove the emulator class. Move the flag and usage code to SDL2 platform.

--- platform/sdl2/sdl2.cpp
+++ platform/sdl2/sdl2.cpp
@@ -1,7 +1,9 @@
 #include <SDL.h>
+#include <cstdint>
 
-#include "emulator.hpp"
 #include "sdl2.hpp"
+
+uint8_t activeColorPalette = 0;
 
 PlatformSdl2::PlatformSdl2() {
 	if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
@@ -42,8 +44,71 @@ PlatformSdl2::PlatformSdl2() {
 	}
 }
 
+void printUsage(const char *argv0, const bool isHelpMessage) {
+	std::cout << "Usage: " << argv0 << " [options] <ROM path>" << std::endl;
+	std::cout << "Options:" << std::endl;
+	std::cout << "\t -s, --serial\t\tEnable serial output" << std::endl;
+	std::cout << "\t -f, --no-fps\t\tDisable 59.70fps limit" << std::endl;
+	std::cout << "\t -d, --debug\t\tEnable debug output" << std::endl;
+	std::cout << "\t -i, --skip-intro\tSkip boot intro" << std::endl;
+	std::cout << "\t -h, --help\t\tShow " << (isHelpMessage ? "this" : "the") << " help message and exit" << std::endl;
+}
+
 int main(int argc, char *argv[]) {
+	if(argc < 2) {
+		printUsage(argv[0], false);
+		return EXIT_FAILURE;
+	}
+
+	const char *romPath = NULL;
+
+	for(size_t i = 1; i < argc; i++) {
+		const char *arg = argv[i];
+
+		if(!strcmp(arg, "-s") || !strcmp(arg, "--serial")) {
+			Config::serialOutput = true;
+		} else if(!strcmp(arg, "-f") || !strcmp(arg, "--no-fps")) {
+			Config::limitFps = false;
+		} else if(!strcmp(arg, "-d") || !strcmp(arg, "--debug")) {
+			Config::debugOutput = true;
+		} else if(!strcmp(arg, "-h") || !strcmp(arg, "--help")) {
+			printUsage(argv[0], true);
+			return EXIT_SUCCESS;
+		} else if(!strcmp(arg, "-i") || !strcmp(arg, "--skip-intro")) {
+			Config::skipIntro = true;
+		} else {
+			romPath = arg;
+		}
+	}
+
+	if(romPath == NULL) {
+		std::cout << "You must provide the path to a valid Game Boy ROM file." << std::endl;
+		printUsage(argv[0], false);
+		return EXIT_FAILURE;
+	}
+
 	PlatformSdl2 platform;
-	Emulator emulator(platform);
-	return emulator.run(argc, argv);
+
+	GameBoy gb(romPath, platform);
+	gb.debugCartridge();
+
+	platform.addGameBoy(&gb);
+
+	while(Config::skipIntro && !gb.introEnded()) {
+		gb.tick();
+	}
+
+	while(platform.running()) {
+		platform.beforeFrame();
+
+		int cycles = 0;
+		while(cycles < GameBoy::CYCLES_PER_FRAME) {
+			cycles += gb.tick();
+		}
+
+		platform.showFrame();
+		platform.afterFrame();
+	}
+
+	return EXIT_SUCCESS;
 }
