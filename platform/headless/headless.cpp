c6e4a82ce5d61e76f4cd8853d3e909534a9fb7bf
keelus <hugomoreda@hotmail.com>
2026-05-16 18:36:47 +0200

    Add a headless platform for debugging.

---
+++ platform/headless/headless.cpp
@@ -0,0 +1,90 @@
+#include <csignal>
+#include <cstdint>
+
+#include "headless.hpp"
+#include <zirc/config.hpp>
+
+uint8_t activeColorPalette = 0;
+
+void printUsage(const char *argv0, const bool isHelpMessage) {
+	std::cout << "Usage: " << argv0 << " [options] <ROM path>" << std::endl;
+	std::cout << "Options:" << std::endl;
+	std::cout << "\t -b, --boot-rom\t\tPath to a custom boot ROM" << std::endl;
+	std::cout << "\t -s, --serial\t\tEnable serial output" << std::endl;
+	std::cout << "\t -f, --no-fps\t\tDisable 59.70fps limit" << std::endl;
+	std::cout << "\t -d, --debug\t\tEnable debug output" << std::endl;
+	std::cout << "\t -i, --skip-intro\tSkip boot intro" << std::endl;
+	std::cout << "\t -h, --help\t\tShow " << (isHelpMessage ? "this" : "the") << " help message and exit" << std::endl;
+}
+
+std::atomic<bool> running = true;
+void exitHandler(int signal) {
+	if(signal == SIGINT) {
+		std::cout << "Exiting..." << std::endl;
+		running = false;
+	}
+}
+
+int main(int argc, char *argv[]) {
+	if(argc < 2) {
+		printUsage(argv[0], false);
+		return EXIT_FAILURE;
+	}
+
+	const char *romPath = NULL;
+
+	for(size_t i = 1; i < argc; i++) {
+		const char *arg = argv[i];
+
+		if(!strcmp(arg, "-b") || !strcmp(arg, "--boot-rom")) {
+			assert(i + 1 < argc && "There was no boot ROM provided after the argument.");
+			Config::useCustomBootRom = true;
+			Config::customBootRomPath = argv[++i];
+		} else if(!strcmp(arg, "-f") || !strcmp(arg, "--no-fps")) {
+			Config::limitFps = false;
+		} else if(!strcmp(arg, "-d") || !strcmp(arg, "--debug")) {
+			Config::debugOutput = true;
+		} else if(!strcmp(arg, "-h") || !strcmp(arg, "--help")) {
+			printUsage(argv[0], true);
+			return EXIT_SUCCESS;
+		} else if(!strcmp(arg, "-i") || !strcmp(arg, "--skip-intro")) {
+			Config::skipIntro = true;
+		} else {
+			romPath = arg;
+		}
+	}
+
+	std::signal(SIGINT, exitHandler);
+
+	if(romPath == NULL) {
+		std::cout << "You must provide the path to a valid Game Boy ROM file." << std::endl;
+		printUsage(argv[0], false);
+		return EXIT_FAILURE;
+	}
+
+	PlatformHeadless platform;
+
+	GameBoy gb(romPath, platform);
+	if(Config::useCustomBootRom) { gb.loadCustomBootRom(Config::customBootRomPath); }
+	gb.debugCartridge();
+
+	platform.addGameBoy(&gb);
+
+	while(Config::skipIntro && !gb.introEnded()) {
+		gb.tick();
+	}
+
+	while(running) {
+		platform.beforeFrame();
+
+		int cycles = 0;
+		while(cycles < GameBoy::CYCLES_PER_FRAME) {
+			cycles += gb.tick();
+		}
+
+		platform.showFrame();
+		platform.afterFrame();
+	}
+
+	return EXIT_SUCCESS;
+}
---
+++ platform/headless/headless.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <cassert>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+
+#include <zirc/audio/ringbuffer.hpp>
+#include <zirc/config.hpp>
+#include <zirc/gameboy.hpp>
+#include <zirc/joypad.hpp>
+#include <zirc/platform.hpp>
+
+#define SCALE 5
+#define FRAME_BUFFER_SIZE (Lcd::WIDTH * Lcd::HEIGHT)
+
+extern uint8_t activeColorPalette;
+
+class PlatformHeadless : public Platform {
+  public:
+	bool running() const override { return m_running; }
+	void drawPixel(uint8_t x, uint8_t y, Color color) override {}
+
+	void beforeFrame() override {}
+	void afterFrame() override {}
+	void swapBuffers() override {}
+	void showFrame() override {}
+
+	float getAudioAmplitude() const override { return 1; }
+	float getAudioSampleRate() const override { return AUDIO_SAMPLE_RATE; }
+
+	void pushAudioSample(float sample) override {}
+
+	void muteAudio() override {}
+	void unmuteAudio() override {}
+
+  private:
+	uint8_t m_nextActiveColorPalette = activeColorPalette;
+
+	bool m_running = true;
+
+	static constexpr float AUDIO_SAMPLE_RATE = 44100.0;
+	static constexpr size_t AUDIO_SAMPLE_AMOUNT = 1024;
+};
